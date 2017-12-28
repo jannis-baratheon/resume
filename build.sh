1d8d61f70b7d1576d1b1ccf8bace166529af5852
Janusz Wiśniowski <wisniaaa@gmail.com>
2017-12-28 15:15:58 +0100

    progress

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO docs/resume.all
+node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO docs/resume.all -t node_modules/jsonresume-theme-flat
