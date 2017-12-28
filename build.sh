c8bfe26eb805a0e6cf09a5fa61702e013f866b2e
Janusz Wiśniowski <wisniaaa@gmail.com>
2017-12-28 14:30:37 +0100

    Changing build output location

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO doc/resume.all
+node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO docs/resume.all
