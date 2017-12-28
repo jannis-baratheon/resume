f6cc1b77a4e02234d780da8e3f5655ab2222dc2d
Janusz Wiśniowski <wisniaaa@gmail.com>
2017-12-28 15:26:54 +0100

    regenerating CVs

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO docs/resume.all -t node_modules/jsonresume-theme-flat
+rm -rf out
+node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO out/resume.all -t node_modules/jsonresume-theme-flat
+cp out/resume.html out/resume.pdf docs
