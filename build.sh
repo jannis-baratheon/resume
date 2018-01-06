04e91764d1a2a331f38fe27ce8b77d8ec4fb46a2
Janusz Wiśniowski <wisniaaa@gmail.com>
2018-01-06 21:29:18 +0100

    Using a custom flat theme to generate html (so that the links work when it's embedded in an iframe)

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 rm -rf out
-node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO out/resume.all -t node_modules/jsonresume-theme-flat
+node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO out/resume.html -t node_modules/jsonresume-theme-flat-custom
+node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO out/resume.pdf -t node_modules/jsonresume-theme-flat
 cp out/resume.html out/resume.pdf docs
