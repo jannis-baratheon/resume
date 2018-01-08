2e293226b9e4b398f363b7a9bb6350b6abd3071e
Janusz Wiśniowski <wisniaaa@gmail.com>
2018-01-08 11:01:10 +0100

    Refactoring project structure. Adding bootstrap4 to index

--- build.sh
+++ build.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
 rm -rf out
+rm -rf target
+
 node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO out/resume.html -t node_modules/jsonresume-theme-flat-custom
 node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO out/resume.pdf -t node_modules/jsonresume-theme-flat
-cp out/resume.html out/resume.pdf docs
+
+mkdir -p target/resumes
+mkdir -p target/vendor/css
+mkdir -p target/vendor/js
+
+cp src/index.html target
+cp out/resume.html out/resume.pdf target/resumes
+
+# css
+cp node_modules/bootstrap/dist/css/bootstrap.min.css target/vendor/css
+
+# js
+cp node_modules/bootstrap/dist/js/bootstrap.min.js target/vendor/js
+cp node_modules/jquery/dist/jquery.slim.min.js target/vendor/js
+cp node_modules/popper.js/dist/umd/popper.min.js target/vendor/js
