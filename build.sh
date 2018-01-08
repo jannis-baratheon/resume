a369e414c476e363d890b094eedf322ee82f7678
Janusz Wiśniowski <wisniaaa@gmail.com>
2018-01-08 12:15:49 +0100

    Bugfixing: iframe focus on page load, page title

--- build.sh
+++ build.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
+# clean
 rm -rf out
 rm -rf target
 
+# build resume
 node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO out/resume.html -t node_modules/jsonresume-theme-flat-custom
 node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO out/resume.pdf -t node_modules/jsonresume-theme-flat
 
+# create target directory structure
 mkdir -p target/resumes
 mkdir -p target/css
+mkdir -p target/js
 mkdir -p target/vendor/css
 mkdir -p target/vendor/js
 
 cp src/index.html target
 cp src/style.css target/css
+cp src/script.js target/js
 cp out/resume.html out/resume.pdf target/resumes
 
 # css
